9d67be4c86ff156c0fb2792c1c1b8e27545b1a20
Juan Restrepo <juan.restrepo023@gmail.com>
2026-02-07 18:36:53 -0500

    Movies add and Home view edited

--- .venv/bin/activate.csh
+++ .venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/juandev22/Documents/EAFIT/ProyectoIntegrador01/Taller01/MOVIEREVIEWSPROJECT/.venv
+setenv VIRTUAL_ENV /home/juandev22/Documents/EAFIT/Proyecto_Integrador_01/Taller01/MOVIEREVIEWSPROJECT/.venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+from .models import Movie
+
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page</h1>')
     # return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Juan José Restrepo Cardona'})
+    #return render(request, 'home.html', {'name':'Juan José Restrepo Cardona'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 # Create your views here.
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
