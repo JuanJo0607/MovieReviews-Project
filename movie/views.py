4b96c0fc367e3a488a6fded5823b89dfb36309e0
Juan Restrepo <juan.restrepo023@gmail.com>
2026-02-07 19:57:17 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+from .models import Movie
+
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page</h1>')
     # return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Juan José Restrepo Cardona'})
-    
+    #return render(request, 'home.html', {'name':'Juan José Restrepo Cardona'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 # Create your views here.
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
